f7e3402a44cf8da796b4a47e325533ceeb222cbb
PASUPUREDDY VAMSI KRISHNA <2300031005@kluniversity.in>
2025-05-11 03:55:14 +0530

    Updated API routes and fixed DB connection

--- models/blackListToken.model.js
+++ models/blackListToken.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const blacklistTokenSchema = new mongoose.Schema({
+const blackListTokenSchema = new mongoose.Schema({
     token: {
         type: String,
         required: true,
@@ -13,4 +13,4 @@ const blacklistTokenSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('BlackListToken', blacklistTokenSchema);
+module.exports = mongoose.model('BlackListToken', blackListTokenSchema);
